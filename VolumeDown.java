307365b69953557e292296e8e9a9f5643c1edc6a
Karla Mae Ibuig <karlamaemibuig@gmail.com>
2026-09-03 13:45:10 +0000

    volume down music player

---
+++ VolumeDown.java
@@ -0,0 +1,12 @@
+public class VolumeDown implements Command {
+    private MusicPlayer musicPlayer;
+
+    public VolumeDown(MusicPlayer musicPlayer) {
+        this.musicPlayer = musicPlayer;
+    }
+
+    @Override
+    public String execute() {
+        return musicPlayer.volumeDown();
+    }
+}
