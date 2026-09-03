2344a285e2c5bbe738b4bb29a93c872aa89b6c03
Karla Mae Ibuig <karlamaemibuig@gmail.com>
2026-09-03 13:37:27 +0000

    Reducing temperature

---
+++ ReducingThermostat.java
@@ -0,0 +1,12 @@
+public class ReducingThermostat implements Command {
+    private Thermostat thermostat;
+
+    public ReducingThermostat(Thermostat thermostat) {
+        this.thermostat = thermostat;
+    }
+
+    @Override
+    public String execute() {
+        return thermostat.reduceTemperature();
+    }
+}
