81de40d93adb570491feedd3cad996d5e9d77c15
Karla Mae Ibuig <karlamaemibuig@gmail.com>
2026-09-03 13:38:50 +0000

    Volume up music player

---
+++ VolumeUp.java
@@ -0,0 +1,12 @@
+public class VolumeUp implements Command {
+    private MusicPlayer musicPlayer;
+
+    public VolumeUp(MusicPlayer musicPlayer) {
+        this.musicPlayer = musicPlayer;
+    }
+
+    @Override
+    public String execute() {
+        return musicPlayer.volumeUp();
+    }
+}
