7d79f32b50a4fda6a4876ece046f16e67ac447fd
Karla Mae Ibuig <karlamaemibuig@gmail.com>
2026-09-03 13:21:36 +0000

    Light

---
+++ Light.java
@@ -0,0 +1,13 @@
+public class Light {
+    private int brightness;
+
+    public String turnOn() {
+        this.brightness = 100;
+        return "Light is ON (Brightness: )" + brigthness + "%");
+    }
+
+    public String turnOff() {
+        this.brightness = 0;
+        return "Light is OFF!";
+    }
+}
